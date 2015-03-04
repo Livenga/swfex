5196b33ac1d247962437399c792d98fe392b0d28
Livenga <ljmmz0405@gmail.com>
2015-03-04 23:27:42 +0900

    include files

--- include/swfex.h
+++ include/swfex.h
@@ -1,5 +1,6 @@
 /* src/graphics/swfjpg.c */
 extern int swf_definebitsjpeg(int fd, const int tag_size);
+extern int swf_definebitsjpeg2(int fd, const int tag_size);
 extern int swf_definebitsjpeg3(int fd, const int tag_size);
 extern int swf_definebitslossless2(int fd, const int tag_number, const int tag_size);
 /* src/sound/swfsound.c */
