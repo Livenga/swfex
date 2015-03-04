a534ab3db30847209cccafa037007a1442ee4eab
Livenga <ljmmz0405@gmail.com>
2015-03-04 17:34:45 +0900

    include files

---
+++ include/swfex.h
@@ -0,0 +1,9 @@
+/* src/graphics/swfjpg.c */
+extern int swf_definebitsjpeg(int fd, const int tag_size);
+extern int swf_definebitsjpeg3(int fd, const int tag_size);
+extern int swf_definebitslossless2(int fd, const int tag_number, const int tag_size);
+/* src/sound/swfsound.c */
+extern int swf_definesound(int fd, const int tag_number, const int tag_size);
+
+/* src/swf/swftag.c */
+extern char *swf_tag_function(const int number);
---
+++ include/swfutil.h
@@ -0,0 +1,3 @@
+/* src/util/util.c */
+extern int remove_temporary(const char *path);
+extern int inflate_zlib(int fd, const char *output_path);
